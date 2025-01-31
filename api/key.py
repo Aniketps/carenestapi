d8aa020d62b84d0264644f1d77bea68d7d444a7f
Aniketps <aniketp2327@gmail.com>
2025-01-31 21:35:26 +0530

    init

---
+++ api/key.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__, template_folder="../templates")
+
+@app.route("/", methods=["GET"])
+def main():
+    return render_template("index.html")
+
+@app.route("/test", methods=["GET"])
+def test():
+    return jsonify({
+        "response" : "Success"
+    })
+
+@app.route("/key", methods=["GET"])
+def key():
+    return jsonify({
+        "response" : "Finished"
+    })
+    
+if __name__ == "__main__":
+    app.run(debug=True)
